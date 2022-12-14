2dab8ef08a7c1bea0b13ccc9c0ecff5785f94363
Andrew <103659333+Adreyko@users.noreply.github.com>
2022-12-14 19:24:55 +0200

    Andriy Soroka commit

---
+++ src/component/Card.tsx
@@ -0,0 +1,18 @@
+import React,{FC} from 'react'
+import {PropsWithChildren} from 'react'
+
+
+interface CardProps {
+    width? : string;
+    height? : string;
+    
+}
+const Card: FC<PropsWithChildren<CardProps>> = ({width,height, children }) => {
+  return (
+    <div style={{height,width,border : '1px solid gray'}}>
+        {children}
+    </div>
+  )
+}
+
+export default Card
